56d80132d95a2074ed45145e5bd45697e86ac6ab
Ajahks <arrendeman@gmail.com>
2024-01-23 01:08:19 -0800

    Add encryption and decryption

---
+++ encryption/encryption.go
@@ -0,0 +1,78 @@
+package encryption
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/gob"
+	"log"
+)
+
+func EncryptCredentials(masterPassword string, credentials map[string]string) []byte {
+    byteBuffer := new(bytes.Buffer)
+    e := gob.NewEncoder(byteBuffer)
+    err := e.Encode(credentials)
+    if err != nil {
+       log.Fatal("Failed to encode credentials")
+    }
+    credentialBytes := byteBuffer.Bytes()
+
+    h := sha256.New()
+    h.Write([]byte(masterPassword))
+    passwordHash := h.Sum(nil)
+
+    aesCipher, err := aes.NewCipher(passwordHash)
+    if err != nil {
+        log.Fatal("Error found when initializing cipher with private key", err)
+    }
+    gcm, err := cipher.NewGCM(aesCipher)
+    if err != nil {
+        log.Fatal("Error while initializing GCM cipher", err)
+    }
+    nonce := make([]byte, gcm.NonceSize())
+    _, err = rand.Read(nonce)
+    if err != nil {
+        log.Fatal("Error while creating random nonce value", err)
+    }
+
+    ciphertext := gcm.Seal(nonce, nonce, credentialBytes, nil)
+
+    return ciphertext 
+}
+
+func DecryptCredentials(masterPassword string, encryptedCredentials []byte) map[string]string {
+    h := sha256.New()
+    h.Write([]byte(masterPassword))
+    passwordHash := h.Sum(nil)
+
+    aesCipher, err := aes.NewCipher(passwordHash)
+    if err != nil {
+        log.Fatal("Error found when initializing cipher with private key", err)
+    }
+    gcm, err := cipher.NewGCM(aesCipher)
+    if err != nil {
+        log.Fatal("Error while initializing GCM cipher", err)
+    }
+
+    nonceSize := gcm.NonceSize()
+    nonce, encryptedCredentials := encryptedCredentials[:nonceSize], encryptedCredentials[nonceSize:]
+
+    decryptedCredentials, err := gcm.Open(nil, nonce, encryptedCredentials, nil)
+    if err != nil {
+        log.Fatal("Error while decoding credentials", err)
+    }
+
+    byteBuffer := new(bytes.Buffer)
+    byteBuffer.Write(decryptedCredentials)
+    var decodedMap map[string]string
+    decoder := gob.NewDecoder(byteBuffer)
+
+    err = decoder.Decode(&decodedMap)
+    if err != nil {
+        log.Fatal("Error while decoding decryptedCrednetials into map", err)
+    }
+
+    return decodedMap
+}
