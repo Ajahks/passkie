47ea2bef36f103fa758cdd02add27b9caca44c90
Arren De Manuel <arrendeman@gmail.com>
2024-02-27 00:11:21 -0800

    [PAS-20] SetPasswordForNewUser func

--- passwordVerification/passwordVerification.go
+++ passwordVerification/passwordVerification.go
@@ -2,11 +2,30 @@ package passwordverification
 
 import (
 	"bytes"
+	"errors"
 
 	"github.com/Ajahks/Passkie/passwordVerification/hash"
 	"github.com/Ajahks/Passkie/passwordVerification/salt"
+	"github.com/Ajahks/Passkie/passwordVerification/storage/local/activeUserDb"
 	passwordDB "github.com/Ajahks/Passkie/passwordVerification/storage/local/passwordHash"
 )
+
+func SetPasswordForNewUser(masterPassword string, username string) error {
+    unsaltedUserHash := hash.HashUsername(username, "")
+    if activeuserdb.IsUserHashActive(string(unsaltedUserHash)) {
+        return errors.New("Failed to set password for new user: User already exists")
+    }
+    activeuserdb.AddActiveUser(string(unsaltedUserHash))
+
+    userhash := hash.HashUsername(username, masterPassword)
+    usersalt := salt.GetSaltForUserHash(userhash)
+    salt.PutSaltForUserHash(userhash, usersalt)
+
+    passwordhash := hash.HashPassword(masterPassword, usersalt)
+    passwordDB.PutPasswordHash(string(userhash), passwordhash)
+
+    return nil
+}
 
 func VerifyPasswordForUser(masterPassword string, username string) bool {
     hashedUser := hash.HashUsername(username, masterPassword)
